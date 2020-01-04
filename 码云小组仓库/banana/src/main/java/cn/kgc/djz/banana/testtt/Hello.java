99a3016771c4e2dc478bd38640a09f832b3abec6
徐渐飞 <2775621261@qq.com>
2020-01-04 15:46:48 +0800

    第一次提交

---
+++ 码云小组仓库/banana/src/main/java/cn/kgc/djz/banana/testtt/Hello.java
@@ -0,0 +1,11 @@
+package cn.kgc.djz.banana.testtt;
+
+/**
+ * @author : djz
+ * @date : Created in  2019/12/27 20:11
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
