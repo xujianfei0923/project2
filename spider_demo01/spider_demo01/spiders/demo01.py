504deb2530b25a7734c276b8ad344fb067ef6709
徐渐飞 <2775621261@qq.com>
2020-09-15 15:42:36 +0800

    爬虫练习小demo

---
+++ spider_demo01/spider_demo01/spiders/demo01.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class Demo01Spider(scrapy.Spider):
+    # name = 'demo01'
+    # allowed_domains = ['http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word='+word+'&ct=201326592&v=flip']
+    # start_urls = ['http://www.qq.com/']
+    #
+    def parse(self, response):
+        pass
